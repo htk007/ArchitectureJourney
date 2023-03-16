217b518ae8afe3273b5f58fe425b09867b478522
htk007 <hasankaya921@hotmail.com>
2023-03-16 21:23:48 +0300

    add unit test for DateTimeHelperTest

---
+++ app/src/test/java/com/heka/firstsamplemvc/Controller/DateTimeHelperTest.java
@@ -0,0 +1,18 @@
+package com.heka.firstsamplemvc.Controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class DateTimeHelperTest {
+
+
+    @Test
+    public void isDateTimeNotEmpty(){
+        String result = DateTimeHelper.getCurrentDateTime();
+        boolean expectedResult = result.isEmpty();
+        assertEquals(expectedResult, false);
+    }
+}
